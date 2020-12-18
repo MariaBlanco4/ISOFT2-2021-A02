0f09818eb52758bc2203e31e4b1fd584c570f128
Maria Blanco <72667900+MariaBlanco4@users.noreply.github.com>
2020-12-18 14:27:10 +0100

    Implementation IT3
    The rest of the files that couldn't be uploadede the fist time because it reached the max number of files.

---
+++ es.uclm.esi.isoft2.a02.restaurant/es.uclm.esi.isoft2.a02.restaurant.making_orders/src/main/java/es/uclm/esi/isoft2/a02/restaurant/making_orders/domain/Notification.java
@@ -0,0 +1,114 @@
+package es.uclm.esi.isoft2.a02.restaurant.making_orders.domain;
+
+import java.sql.SQLException;  
+import java.util.Vector;
+
+import es.uclm.esi.isoft2.a02.restaurant.dependencies.persistence.Broker;
+
+
+public class Notification {
+
+	private int origin;
+	private String message;
+	private int destination;
+	private int id_notification; 
+
+	public int getOrigin() {
+		return this.origin;
+	}
+
+	public void setOrigin(int origin) {
+		this.origin = origin;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getDestination() {
+		return this.destination;
+	}
+
+	public void setDestination(int destination) {
+		this.destination = destination;
+	}
+
+	public int getId_notification() {
+		return this.id_notification;
+	}
+
+	public void setId_notification(int id_notification) {
+		this.id_notification = id_notification;
+	}
+
+	/**
+	 * 
+	 * @param destination
+	 */
+	public static Notification[] readNotification (int destination) throws SQLException, Exception { 
+		String sql = "SELECT id_notification, message, origin, destination FROM Notification WHERE destination= " + destination + ";";
+		Vector<Object> vBroker = Broker.getBroker().select(sql);
+		Notification[] auxNot = new Notification[(vBroker.size())];
+		Vector<Object> auxVector;
+
+		auxVector = new Vector<Object>();
+
+		if (vBroker.size() >= 1) {
+			for (int i = 0; i < (vBroker.size()); i ++) {
+				auxVector = (Vector<Object>) vBroker.elementAt(i);
+				auxNot[i] = new Notification((Integer) auxVector.elementAt(2), (String) auxVector.elementAt(1), (Integer) auxVector.elementAt(3),(Integer)  auxVector.elementAt(0));
+				System.out.println(auxNot[i].getMessage());
+			}
+		}
+		
+		return auxNot; 
+	}
+	
+	public Notification(int origin, String message, int destination, int id_notification) {
+		super();
+		this.origin = origin;
+		this.message = message;
+		this.destination = destination;
+		this.id_notification = id_notification;
+	}
+
+	/**
+	 * 
+	 * @param id_notification
+	 */
+	public static int deleteNotification(int id_notification) throws SQLException, Exception{
+		String sql = "DELETE FROM Notification WHERE id_notification =" + id_notification + ";";
+		Broker.getBroker();
+		return Broker.delete(sql);
+	}
+	
+	/** 
+	 * @param message
+	 * @param origin
+	 * @param destination
+	 *	
+	 */
+	public static int insertNotification(String message, int origin, int destination) throws Exception{
+		String sql = "INSERT INTO Notification (message, origin, destination) VALUES('"+message+"',"+origin+","+destination+");";
+		Broker.getBroker();
+		int nt = 0;
+		try {
+			nt=Broker.insert(sql);
+		} catch (Exception e) {
+			nt=0;
+		}
+		return nt;
+	}
+	
+	/*
+	 * Constructor to get the notification
+	 */
+	public Notification(int destination) {
+		super();
+		this.destination = destination;
+	}
+}
---
+++ es.uclm.esi.isoft2.a02.restaurant/es.uclm.esi.isoft2.a02.restaurant.table_booking/src/main/java/es/uclm/esi/isoft2/a02/restaurant/table_booking/domain/Table.java
@@ -0,0 +1,52 @@
+package es.uclm.esi.isoft2.a02.restaurant.table_booking.domain;
+
+import java.sql.SQLException;
+import java.util.Vector;
+
+import es.uclm.esi.isoft2.a02.restaurant.dependencies.domain.Turn;
+import es.uclm.esi.isoft2.a02.restaurant.dependencies.persistence.Broker;
+
+
+
+public class Table {
+
+	private int n_table;
+	private int n_chairs;
+	private Turn turn;
+
+	
+	public Table(int n_table, int n_chairs, Turn turn) {
+		this.n_table = n_table;
+		this.n_chairs = n_chairs;
+		this.turn = turn;
+	}
+	
+	
+	/**
+	 * 
+	 * @param turn
+	 * @param n_chairs
+	 * @throws Exception 
+	 * @throws SQLException 
+	 */
+	public static Table[] read(Turn turn, int n_chairs) throws SQLException, Exception {
+		String sql = "SELECT * FROM Table_t WHERE turn='" + turn + "' AND n_chairs=" + n_chairs + ";";
+		Vector<Object> auxVector;
+		Vector<Object> vBroker = Broker.getBroker().select(sql);
+		Table [] auxTable = new Table[vBroker.size()];
+
+		auxVector = new Vector<Object>();
+
+		if (vBroker.size() >= 1) {
+			for(int i=0; i<vBroker.size();i++) {
+				auxVector = (Vector<Object>) vBroker.elementAt(i);
+				auxTable[i] = new Table((Integer) auxVector.elementAt(0), (Integer) auxVector.elementAt(1), Turn.valueOf(Turn.class, auxVector.elementAt(2).toString()));
+			}
+		}
+		return auxTable;
+	}
+	
+	public int getN_table() {
+		return this.n_table;
+	}
+}
