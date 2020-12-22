eed516c33b755397734ed660c2f2a45350d956aa
AdrianRuiz6 <66148706+AdrianRuiz6@users.noreply.github.com>
2020-12-22 22:12:05 +0100

    changes made in making_orders/tests

--- es.uclm.esi.isoft2.a02.restaurant/es.uclm.esi.isoft2.a02.restaurant.making_orders/src/test/java/es/uclm/esi/isoft2/a02/restaurant/making_orders/domain/DishTest.java
+++ es.uclm.esi.isoft2.a02.restaurant/es.uclm.esi.isoft2.a02.restaurant.making_orders/src/test/java/es/uclm/esi/isoft2/a02/restaurant/making_orders/domain/DishTest.java
@@ -6,7 +6,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import es.uclm.esi.isoft2.a02.restaurant.making_orders.domain.Dish;
+
 
 public class DishTest {
 
