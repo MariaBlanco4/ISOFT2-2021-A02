7b07dd1b14b9be72a65e1f49c15a58a882ba73ea
AdrianRuiz6 <66148706+AdrianRuiz6@users.noreply.github.com>
2020-12-22 20:05:04 +0100

    solved dependencies/UserTest

--- es.uclm.esi.isoft2.a02.restaurant/es.uclm.esi.isoft2.a02.restaurant.dependencies/src/test/java/es/uclm/esi/isoft2/a02/restaurant/dependencies/domain/UserTest.java
+++ es.uclm.esi.isoft2.a02.restaurant/es.uclm.esi.isoft2.a02.restaurant.dependencies/src/test/java/es/uclm/esi/isoft2/a02/restaurant/dependencies/domain/UserTest.java
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-package es.uclm.esi.isoft2.a02.restaurant.dependencies.domain;
-
-import static org.junit.Assert.*;
-
-import java.sql.SQLException;
-
-import org.junit.Test;
-
-public class UserTest {
-	
-	User u[];
-	User uaux;
-
-	@Test
-	public void testReadPosition() throws SQLException, Exception {
-		assertEquals(u,User.readPosition("cooker"));
-	}
-
-	@Test
-	public void testReadUser() throws SQLException, Exception {
-		assertEquals(uaux,User.readUser(1, "jose", "waiter"));
-	}
-
-	@Test
-	public void testRead() throws SQLException, Exception {
-		assertEquals(uaux,User.read(2));
-	}
-
-}
-=======
 package es.uclm.esi.isoft2.a02.restaurant.dependencies.domain;
 
 import static org.junit.Assert.*;
@@ -75,4 +44,4 @@ public class UserTest {
 	}
 
 }
->>>>>>> Merge
+
