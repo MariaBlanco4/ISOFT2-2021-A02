147a37321d9c90e15e6d702d3fc10591d15dbbab
AdrianRuiz6 <66148706+AdrianRuiz6@users.noreply.github.com>
2020-12-22 20:03:40 +0100

    solved dependencies/orderTest

--- es.uclm.esi.isoft2.a02.restaurant/es.uclm.esi.isoft2.a02.restaurant.dependencies/src/test/java/es/uclm/esi/isoft2/a02/restaurant/dependencies/domain/OrderTest.java
+++ es.uclm.esi.isoft2.a02.restaurant/es.uclm.esi.isoft2.a02.restaurant.dependencies/src/test/java/es/uclm/esi/isoft2/a02/restaurant/dependencies/domain/OrderTest.java
@@ -8,10 +8,6 @@ import org.junit.Test;
 
 import es.uclm.esi.isoft2.a02.restaurant.dependencies.persistence.Broker;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> Merge
 public class OrderTest {
 	
 	private static Order order0;
